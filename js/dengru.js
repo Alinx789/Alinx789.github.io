642a1f868d8c831954f623b2e1d50f61ea6512fa
Alinx789 <1719162145@qq.com>
2023-01-04 12:53:57 +0800

    'upload'

---
+++ js/dengru.js
@@ -0,0 +1,30 @@
+ var curIndex = 0;
+
+var timeInterval = 5000;
+                       
+var arr = new Array();
+arr[0] = "img/csll/1.jpg";
+arr[1] = "img/csll/2.jpg";
+arr[2] = "img/quyu/taizihequ.jpg";
+setInterval(changeImg, timeInterval);
+function changeImg() {   
+     if (curIndex == arr.length - 1) {
+     curIndex = 0;
+     } 
+	else{
+     curIndex += 1;
+     }
+document.body.style.backgroundImage = "URL("+arr[curIndex]+")";  
+}
+function myFunction(){
+if(document.getElementById("user").value=="admin"){
+if(document.getElementById("password").value=="123456"){
+			alert("登陆成功");
+		}
+	}
+	else{
+		alert("账号或者密码错误");
+	}
+}
+
+
